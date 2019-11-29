97b207ddc2233912a9567a9b03d1a376dc50962a
Domenic Fiore <domenicfiore@gmail.com>
2019-11-28 23:02:44 -0500

    It works now but needs a lot of love.

--- app/weather-block/index.js
+++ app/weather-block/index.js
@@ -1,11 +1,13 @@
+import { debounce } from 'debounce';
+
 const { registerBlockType }         =   wp.blocks;
 const { __ }						=   wp.i18n;
 const { InspectorControls, BlockControls, AlignmentToolbar, BlockAlignmentToolbar } = wp.editor;
-const { PanelBody, PanelRow, TextControl, SelectControl } = wp.components;
+const { PanelBody, PanelRow, TextControl, SelectControl, Button } = wp.components;
 
 import Location from './location';
 const location = new Location();
-console.log(location.getLocation('canton, ma').lat);
+location.getLocation('boston, ma');
 import Darksky from 'darkskyjs';
 const darkSky = new Darksky({ PROXY_SCRIPT: window.location.protocol + '//' + window.location.hostname + "/darksky" });
 // const data = darkSky.getCurrentConditions([{latitude: 37.8267, longitude: -122.4233, name: 'Canton'}], function (ok) {
@@ -19,48 +21,97 @@ registerBlockType( 'domenicf/simple-weather-block', {
 	category:							__( 'common' ),
 	icon:								'palmtree',
 	attributes: {
-		zip_code: {
-			type: 'text',
+		location: {
+			type: 'string',
 			default: '02109'
 		},
+		location_options: {
+			type: 'array',
+			default: [{ value: 'search', label: 'Search for a location first.' }]
+		},
+		set_location: {
+			type: 'string',
+			default: '02109'
+		},
+		latitude: {
+			type: 'string'
+		},
+		longitude: {
+			type: 'string'
+		},
 		temperature: {
-			type: 'text'
+			type: 'string',
+			default: ''
+		},
+		location_name: {
+			type: 'string',
+			default: ''
 		}
 	},
 	edit: props => {
-		let weatherResult = darkSky.getCurrentConditions([{latitude: 37.8267, longitude: -122.4233, name: 'Canton'}], function (ok) {
-			props.setAttributes( { temperature: ok[0].apparentTemperature() } );
-			console.log('saved:' + props.attributes.temperature);
-		});
+		let weatherResult = (latitude, longitude) => {
+
+			darkSky.getCurrentConditions([{latitude, longitude, name: props.attributes.set_location}], function (data) {
+				props.setAttributes({ temperature: data[0].temperature().toString() });
+			});
+
+		}
 
 		return [
 			<InspectorControls>
 				<PanelBody title={ __( 'Location', 'simple-weather-block' ) }>
 					<PanelRow>
-						<p>{ __( 'Set the ZIP code here.' ) }</p>
+						<p>{ __( 'Set the location here.' ) }</p>
 					</PanelRow>
 					<TextControl
-						label={ __( 'ZIP Code', 'simple-weather-block' ) }
-						help={ __( 'Enter the ZIP code for the place where you want the weather to be displayed.', 'simple-weather-block' )}
-						value={ props.attributes.zip_code }
+						label={ __( 'Location', 'simple-weather-block' ) }
+						help={ __( 'Enter the location for the place where you want the weather to be displayed.', 'simple-weather-block' )}
+						value={ props.attributes.location }
 						onChange={ new_val => {
-							props.setAttributes( { zip_code: new_val } )
+							props.setAttributes( { location: new_val } )
 						}}
 					/>
+					<Button isDefault onClick={() => {
+						location.getLocation(props.attributes.location).then(response => {
+							let location_options = [{ value: '', label: 'Success! Choose from a location.' }];
+							response.data.forEach(element => {
+								location_options.push({
+									label: element.display_name,
+									value: element.lat + ',' + element.lon,
+								});
+							});
+							props.setAttributes({ location_options });
+							console.log(location_options);
+						});
+					}}>Search</Button>
+					<SelectControl
+						label={ __( 'Location', 'simple-weather-block' ) }
+						help={ __( 'Choose from a list of possible locations.', 'simple-weather-block' ) }
+						value={ props.attributes.set_location }
+						options={ props.attributes.location_options }
+						onChange={ new_val => {
+							let lat_lon = new_val.split(',');
+							props.setAttributes({ set_location: new_val, latitude: lat_lon[0], longitude: lat_lon[1] });
+							weatherResult(lat_lon[0], lat_lon[1]);
+						} }
+					/>
+
 				</PanelBody>
 			</InspectorControls>,
 			<div className={ props.className }>
 				<p>
-					{ props.attributes.temperature }
+					{props.attributes.temperature.length > 0 && 'The current temperature as of this post is ' + props.attributes.temperature + ' in ' + props.attributes.set_location + '.'}
 				</p>
 			</div>
 		];
 	},
 	save: props => {
 		return (
-		<p>
-			{ props.attributes.temperature }
-		</p>
+			<div className={ props.className }>
+				<p>
+					{props.attributes.temperature.length > 0 && 'The current temperature as of this post is ' + props.attributes.temperature + ' in ' + props.attributes.set_location + '.'}
+				</p>
+			</div>
 		);
 	}
 });
--- app/weather-block/location.js
+++ app/weather-block/location.js
@@ -1,21 +1,31 @@
+import axios from 'axios';
+
 export default class Location {
 	constructor() {
 		this.requestDone = false;
 	}
 
 	getLocation(location) {
+		let data = {};
+
+		return axios.get('/swb_location', {
+			params: {
+				location
+			}
+		});
+
 		// create a new XMLHttpRequest
-		var xhr = new XMLHttpRequest()
+		// var xhr = new XMLHttpRequest()
 
 		// get a callback when the server responds
-		xhr.addEventListener('load', () => {
-		  // update the state of the component with the result here
-		  console.log(xhr.responseText)
-		  this.requestDone = true;
-		})
-		// open the request with the verb and the url
-		xhr.open('GET', window.location.protocol + '//' + window.location.hostname + '/swb_location?location=' + location)
-		// send the request
-		xhr.send()
+		// xhr.addEventListener('load', () => {
+		//   // update the state of the component with the result here
+		//   console.log(JSON.parse(xhr.responseText));
+		//   this.requestDone = true;
+		// })
+		// // open the request with the verb and the url
+		// xhr.open('GET', window.location.protocol + '//' + window.location.hostname + '/swb_location?location=' + location)
+		// // send the request
+		// xhr.send()
 	}
 }
